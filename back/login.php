9d593d7dae4558ff4aa7a2f4df1507993d887830
Arthur Moreira da Silva <94649590+arthuurms@users.noreply.github.com>
2024-12-15 12:44:08 -0300

    Add files via upload

--- back/login.php
+++ back/login.php
@@ -1,6 +1,36 @@
 <?php
+    session_start();
     require_once 'funcoes.php';
-    $u=new funcoes("aula", "localhost", "root", "");
+    $u=new funcoes("sql10750543", "sql10.freesqldatabase.com", "sql10750543", "tGXF33BwST");
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $email = $_POST['email'];
+        $password = $_POST['senha'];
+
+        $response_query = "SELECT id, nome, email, senha FROM contas WHERE username = ?";
+        $smt = $pdo->prepare($response_query);
+        $smt -> bind_param("s", $email)
+        $smt -> execute();
+        $smt -> store_result();
+        if ($smt->num_rows > 0){
+            $smt -> bind_result($id, $email, $user_password);
+            $smt ->fetch();
+            if ($password == $user_password){
+                $_SESSION['email'] = $email;
+                #RESTANTE
+            } else {
+                echo "Senha incorreta"
+            }
+
+        } else {
+            echo "Não existe usuário com esse email."
+        }
+
+        $smt->close()
+    }
+
+    $pdo->close()
+
 ?>
 <!DOCTYPE html>
 <html>
